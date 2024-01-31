7e6837361f02d9a9215368c2de077eb2ca99fb63
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-30 19:45:07 -0500

    Erg

--- includes/configManager.h
+++ includes/configManager.h
@@ -1,4 +1,4 @@
-#include <iostream> 
+#include <iostream>
 #include <limits>
 using namespace std;
 
@@ -6,78 +6,77 @@ using namespace std;
 #define CONFIGMANAGER_H
 
 class ConfigHandler {
-    private: 
-	vector <string> readfile() {
-		
-	}
-	void checkUsername() {
-
-	}
-	void checkPassword() {
-
-	}
-	void checkPin() {
-
-	}
-    public: 
-    void createConfigFile(string filename) {
-	ofstream newFile(filename);
-	if (!newFile.is_open()) {
-		char answer;
-		cout << endl << "We are having issues initializing a configuration file for your new account. Would you like to create this configuration file manually?" << endl << " (Y/N) Yes/No: ";
-		cin >> answer;
-		if (cin.fail()) {
-			cin.clear();
-			cin.ignore(numeric_limits < streamsize > ::max());
-			return;
-		}
-		if (answer == 'Y' || answer == 'y') {
-			cout << endl << "Steps to take: " << endl << "1. End this program and within your current directory type in \"touch config.yaml\" " << endl << "2. Run a command to make sure that you have read write and execution access within the directory \"chmod 777 config.yaml\"" << endl << "3. You are all set. Restart the application and try again";
-			return;
-		}
-	}
-	return;
-}
-bool checkForExistingAccount() {
-	ifstream file("config.yaml");
-	bool confirmed = false;
+	private:
+	public:
+bool checkForLocalConfigFile() {
+	string filename = "config.yaml";
+	ifstream file(filename);
 	if (!file.is_open()) {
-		cout << "There was an error creating or opening a new configuration file for your account." << endl << "Would you like to manually go through the steps to fix this problem?" << endl;
-		string confirmation;
-		while (!confirmed) {
-			cin << confirmation;
-			if (cin.fail()) {
-				cin.clear();
-				cin.ignore(numeric_limits <streamsize>::max(), '\n');
-				confirmed = true;
-				return false;
-			}
-			if (confirmation != "Y" || confirmation != "y") {
-				cout << endl << "Steps to take: " << endl << "1. End this program and within your current directory type in \"touch config.yaml\" " << endl << "2. Run a command to make sure that you have read write and execution access within the directory \"chmod 777\"" << endl << "3. You are all set. Restart the application and try again";
-				confirmed = true;
-				return false;
-			}
-		}
-	}
-	string line;
-	string value;
-	getline(file, line);
-	stringstream ss(line);
-	vector <string> rows;
-	while(getline(ss, value, '\n')) {
-		row.push_back(value);
-	}
-	for (const string& row: rows) {
-		cout << row << endl;
-	}
-	if (rows.size() < 1) {
 		return false;
 	}
-	return false;
+	return true;
 }
-void createAccount() {
-	cout << "Let us create you an account." << endl << "Welcome to CPP-Notes" << endl;
-}
+
+	void createConfigFile(string filename) {
+		ofstream newFile(filename);
+		if (!newFile.is_open()) {
+			char answer;
+			cout << endl << "We are having issues initializing a configuration file for your new account. Would you like to create this configuration file manually?" << endl << " (Y/N) Yes/No: ";
+			cin >> answer;
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(numeric_limits < streamsize > ::max());
+				return;
+			}
+			if (answer == 'Y' || answer == 'y') {
+				cout << endl << "Steps to take: " << endl << "1. End this program and within your current directory type in \"touch config.yaml\" " << endl << "2. Run a command to make sure that you have read write and execution access within the directory \"chmod 777 config.yaml\"" << endl << "3. You are all set. Restart the application and try again";
+				return;
+			}
+		}
+		return;
+	}
+
+	bool checkForExistingAccount() {
+		ifstream file("config.yaml");
+		bool confirmed = false;
+		if (!file.is_open()) {
+			cout << "There was an error creating or opening a new configuration file for your account." << endl << "Would you like to manually go through the steps to fix this problem?" << endl;
+			string confirmation;
+			while (!confirmed) {
+				cin << confirmation;
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+					confirmed = true;
+					return false;
+				}
+				if (confirmation != "Y" || confirmation != "y") {
+					cout << endl << "Steps to take: " << endl << "1. End this program and within your current directory type in \"touch config.yaml\" " << endl << "2. Run a command to make sure that you have read write and execution access within the directory \"chmod 777\"" << endl << "3. You are all set. Restart the application and try again";
+					confirmed = true;
+					return false;
+				}
+			}
+		}
+		string line;
+		string value;
+		getline(file, line);
+		stringstream ss(line);
+		vector < string > rows;
+		while(getline(ss, value, '\n')) {
+			row.push_back(value);
+		}
+		for (const string& row: rows) {
+			cout << row << endl;
+		}
+		if (rows.size() < 1) {
+			return false;
+		}
+		return false;
+	}
+
+	void createAccount() {
+		cout << "Let us create you an account." << endl << "Welcome to CPP-Notes" << endl;
+	}
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,146 +14,15 @@ namespace fs = std::filesystem;
 FileManager fileManager;
 ConfigFileManager configManager;
 
-// void initializeAccount() {
-// 	bool getUsername = true;
-// 	bool getPassword = true;
-// 	bool getPasswordConfirm = true;
-// 	bool getPin = false;
-// 	string username;
-// 	string password;
-// 	string passwordConfirm;
-// 	int pin;
-// 	cout << "Lets create a new account for you so you can sync your notes and keep your data safe." << endl << endl;
-// 	cout << "What would you like your new username to be? ";
-// 	while (getUsername) {
-// 		cin >> username;
-// 		if (cin.fail()) {
-// 			cin.clear();
-// 			cin.ignore(numeric_limits < streamsize > ::max(), '\n');
-// 			cout << "Please enter a valid username" << endl << "This username should be at minimum 4 characters" << endl << "Your new username can only include:" << endl << "- A-Z, a-z, numbers, _, and -" << endl;
-// 		}
-// 		if (username.length() < 4) {
-// 			cout << endl << "Your username must be at minimum 4 characters long" << endl;
-// 		}
-// 		getUsername = false;
-// 	}
-// 	cout << "Great choice on username" << endl;
-// 	cout << "You can always change your username later" << endl;
-// 	cout << "Input a password" << endl;
-// 	while (getPassword) {
-// 		cin >> password;
-// 		if (cin.fail()) {
-// 			cin.clear();
-// 			cin.ignore(numeric_limits < streamsize > ::max(), '\n');
-// 			cout << "Please enter a valid password" << endl << "This username should be at minimum 8 characters" << endl << "Your new password must include:" << endl << "2 uppercase letters" <<
-// 			endl << "2 lowercase letters" <<
-// 			endl << "At least 2 numbers" <<
-// 			endl << "At least two special characters" <<
-// 			endl << "And be a total of 8 characters in length" << endl;
-// 		}
-// 		if (password.length() < 8) {
-// 			cout << endl << "Your new password must be at least 8 characters long" << endl;
-// 			return;
-// 		}
-// 		getPassword = false;
-// 	}
-// 	cout << "Confirm your password: ";
-// 	while (getPasswordConfirm) {
-// 		cin >> getPasswordConfirm;
-// 		if (cin.fail()) {
-// 			cin.clear();
-// 			cin.ignore(numeric_limits < streamsize > ::max(), '\n');
-// 			cout << "Please enter a valid username" << endl << "This username should be at minimum 4 characters" << endl << "Your new username can only include:" << endl << "- A-Z, a-z, numbers, _, and -" << endl;
-// 		}
-// 	}
-// }
-
-// void checkForNotesDirectory() {
-// 	string relativePath = "my_notes";
-// 	fs::path absolutePath = fs::absolute(relativePath);
-// 	if (!fs::exists(absolutePath) || !fs::is_directory(absolutePath)) {
-// 		try {
-// 			fs::create_directory(absolutePath);
-// 			return;
-// 		}
-// 		catch (const std::filesystem::filesystem_error& e) {
-// 			char answer;
-// 			cout << e.what() << endl << "There was a problem creating a directory to store your new notes when using this application" << endl << "Would you like to continue and create this directory manually?" << endl << "(Y/N)Yes/No: ";
-// 			cin >> answer;
-// 			if (cin.fail()) {
-// 				cin.clear();
-// 				cin.ignore(numeric_limits < streamsize > ::max());
-// 				return;
-// 			}
-// 			if (answer == 'Y' || answer == 'y') {
-// 				cout << endl << "Steps to take: " << endl << "1. End this program and within your current directory type in \"mkdir my_notes\" " << endl << "2. Run a command to make sure that you have read write and execution access within the directory \"chmod 777\"" << endl << "3. You are all set. Restart the application and try again";
-// 				return;
-// 			}
-// 			return;
-// 		}
-// 		return;
-// 	}
-// 	return;
-// }
-
-// void printMenu() {
-// 	checkForNotesDirectory();
-// 	/*	bool configFileExists = checkForLocalUserAndConfigFile();
-// 	if (!configFileExists) {
-// 		createConfigFile("config.yaml");
-// 		initializeAccount();
-// 	}*/
-// 	cout << "*** Welcome to CPP Notes ***" << endl << endl;
-// 	vector < string > options;
-// 	options.push_back("1. Create account");
-// 	options.push_back("2. Exit");
-// 	int selectedOption;
-// 	for (const auto& pair: options) {
-// 		cout << pair << endl;
-// 	}
-// 	cout << endl << "Option: ";
-// 	cin >> selectedOption;
-// 	if (cin.fail()) {
-// 		cin.clear();
-// 		cin.ignore(numeric_limits < streamsize > ::max(), '\n');
-// 		system("clear");
-// 		cout << endl << "You must pick a valid option from the menu" << endl;
-// 		printMenu();
-// 		return;
-// 	}
-// 	if (selectedOption > 2 || selectedOption < 1) {
-// 		system("clear");
-// 		cout << endl << "You must pick a valid option from the menu" << endl;
-// 		printMenu();
-// 		return;
-// 	}
-// 	switch (selectedOption) {
-// 		case 1:
-// 		return;
-// 		break;
-// 		case 2:
-// 		return;
-// 		break;
-// 	}
-// }
-
-bool checkForLocalConfigFile() {
-	string filename = "config.yaml";
-	ifstream file(filename);
-	if (!file.is_open()) {
-		return false;
-	}
-	return true;
-}
-
 void checkForAccount() {
-	bool configExist = checkForLocalConfigFile();
+	bool configExist = configManager.checkForLocalConfigFile();
 	if (!configExist) {
 		configManager.createConfigFile("config.yaml");
 		configManager.createAccount();
 	}
 	if (configExist) {
-		login();
+		bool accountEstablished = configManager.checkForExistingAccount();
+		//Check if user data in config
 	}
 }
 
