6cd6d01d73e834a14fd5867da8f67853e5453360
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-02-06 09:50:33 -0500

    Menu Options
    Errors:
    Handling errors when user inputs invalid values
    for menu options

--- main.cpp
+++ main.cpp
@@ -14,46 +14,57 @@ namespace fs = std::filesystem;
 FileManager fileManager;
 ConfigManager configManager;
 
-const vector <pair<int, string>> {
-	{
-		1, "1. New Note"
-	}, {
-		2, "2. New Folder",
-	}, {
-		3, "3. Settings"
-	}, {
-		4, "4. Logout"
-	}, {
-		5. "5. Delete Account"
-	}
-}
+const vector < string > options = {
+  "1. New Note",
+  "2. New Folder",
+  "3. Settings",
+  "4. Logout",
+  "5. Delete Account"
+};
 
 void printMenu() {
-	vector <string> userInfo = configManager.getUserInfo();
-	cout << "Welcome " << userInfo[0] << endl;
-	cout << "**************" << end;
-
+  bool getOption = true;
+  vector < string > userInfo = configManager.getUserInfo();
+  cout << "Welcome " << userInfo[0] << endl;
+  cout << "**************" << endl;
+  for (string option: options) {
+    cout << option << endl;
+  }
+  int selection;
+  int tries = 0;
+  while (getOption) {
+    cin >> selection;
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits < streamsize > ::max());
+    if (tries == 3) {
+      return;
+    }
+    cout << "Please select a valid option from the menu. If you are trying to exit the program type 10" << endl;
+    tries++;
+  }
+}
 }
 
 void checkForAccount() {
-	bool configExist = configManager.checkForLocalConfigFile();
-	if (!configExist) {
-		configManager.createConfigFile("config.yaml");
-		configManager.createAccount();
-	}
-	if (configExist) {
-		bool accountEstablished = configManager.checkForExistingAccount();
-		if (accountEstablished) {
-			printMenu();
-		}
-		else {
-			configManager.createAccount();
-		}
-	}
+bool configExist = configManager.checkForLocalConfigFile();
+if (!configExist) {
+configManager.createConfigFile("config.yaml");
+configManager.createAccount();
+}
+if (configExist) {
+bool accountEstablished = configManager.checkForExistingAccount();
+if (accountEstablished) {
+printMenu();
+}
+else {
+configManager.createAccount();
+}
+}
 }
 
 int main(int argc, char* argv[]) {
-	system("clear");
-	checkForAccount();
-	return 0;
+system("clear");
+checkForAccount();
+return 0;
 }
