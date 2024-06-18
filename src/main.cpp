1d6335cfa5663717e4308ed21f6868215f30a18c
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-06-17 22:22:21 -0400

    Switch Case
    Cleaning up if else statments and
    implementing settings features

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ ExceptionHandler exceptionHandler;
 IoHandler ioHandler;
 
 void printMenu();
+void printSettings();
 vector < string > userInfo;
 
 const vector < string > options = {
@@ -32,28 +33,53 @@ const vector < string > options = {
  "6. Delete Account"
 };
 
+const vector < string > settings = {
+ "1. Update profile",
+ "2. Create option",
+ "3. Main menu"
+};
+
+void selectSettingsAction(int option) {
+ switch (option) {
+  case 1:
+  // Update profile
+  break;
+  case 2:
+  // Create option
+  break;
+  case 3:
+  system("clear");
+  printMenu();
+  break;
+  default:
+  break;
+ }
+}
+
 void selectAction(int option) {
- if (option == 1) {
+ switch (option) {
+  case 1:
   system("clear");
   // fileManager.createNewFile("New Note", ".txt");
- }
- else if (option == 2) {
+  break;
+  case 2:
   //fileManager.createNewDir();
- }
- else if (option == 3) {
-  // stateManager.setState("settings");
+  break;
+  case 3:
+  system("clear");
+  printSettings();
   return;
- }
- else if (option == 4) {
+  break;
+  case 4:
   system("clear");
   configManager.logout();
   return;
- }
- else if (option == 5) {
+  break;
+  case 5:
   system("clear");
   return;
- }
- else if (option == 6) {
+  break;
+  case 6:
   system("clear");
   string userInput = ioHandler.getInput < string > (
    {
@@ -67,66 +93,85 @@ void selectAction(int option) {
    configManager.deleteAccount();
    system("clear");
    cout << "Account deleted successfully" << endl;
-   // Choose later if I want the user experience to be to
-   // immediately recreate an account or exit the program
-   // configManager.createAccount();
    return;
   }
   else {
    system("clear");
    printMenu();
+   return;
   }
- }
- else {
+  break;
+  default:
+  system("clear");
   exceptionHandler.printPlainError("Please select a valid option");
   printMenu();
   return;
+  break;
  }
-}
+} 
 
-void printMenu() {
- userInfo = configManager.getUserInfo(false);
- cout << "Welcome " << userInfo[1] << endl;
- cout << "**************" << endl;
- for (const string& option: options) {
-  cout << option << endl;
- }
- int selection = ioHandler.getInput < int > ({{
-  ""
- }}, "Option: ", "Please select a number as an option");
- if (selection < 1 || selection > 6) {
-  exceptionHandler.printPlainError("Please select an available option from the menu. Or you can create a new option in your settings");
-  system("clear");
-  printMenu();
- }
- selectAction(selection);
-}
-
-void checkForAccount() {
- ifstream* config = configManager.checkForLocalConfigFile("config.yaml");
- if (!config) {
-  ofstream* newConfig = configManager.createConfigFile("config.yaml");
-  if (!newConfig) {
+ void printSettings() {
+  for (const string& setting: settings) {
+   cout << setting << endl;
+  }
+  int selection = ioHandler.getInput < int > ({{
+   ""
+  }}, "Option: ", "Please input a valid option from the list");
+  if (selection < 1 || selection > 3) {
+   system("clear");
+   exceptionHandler.printPlainError("Select an available setting, or return to the main menu by pressing 3");
+   printSettings();
    return;
   }
-  configManager.createAccount(newConfig);
-  delete newConfig;
-  checkForAccount();
+  selectSettingsAction(selection);
+  return;
  }
- if (config) {
-  int accountEstablished = configManager.checkForExistingAccount();
-  if (accountEstablished == 1) {
-   return;
+
+ void printMenu() {
+  userInfo = configManager.getUserInfo(false);
+  cout << "Welcome " << userInfo[1] << endl;
+  cout << "**************" << endl;
+  for (const string& option: options) {
+   cout << option << endl;
   }
-  if (accountEstablished == 0) {
+  int selection = ioHandler.getInput < int > ({{
+   ""
+  }}, "Option: ", "Please select a number as an option");
+  if (selection < 1 || selection > 6) {
+   exceptionHandler.printPlainError("Please select an available option from the menu. Or you can create a new option in your settings");
+   system("clear");
    printMenu();
-   delete config;
+   return;
+  }
+  selectAction(selection);
+  return;
+ }
+
+ void checkForAccount() {
+  ifstream* config = configManager.checkForLocalConfigFile("config.yaml");
+  if (!config) {
+   ofstream* newConfig = configManager.createConfigFile("config.yaml");
+   if (!newConfig) {
+    return;
+   }
+   configManager.createAccount(newConfig);
+   delete newConfig;
+   checkForAccount();
+  }
+  if (config) {
+   int accountEstablished = configManager.checkForExistingAccount();
+   if (accountEstablished == 1) {
+    return;
+   }
+   if (accountEstablished == 0) {
+    printMenu();
+    delete config;
+   }
   }
  }
-}
 
-int main(int argc, char* argv[]) {
- system("clear");
- checkForAccount();
- return 0;
-}
+ int main(int argc, char* argv[]) {
+  system("clear");
+  checkForAccount();
+  return 0;
+ }
