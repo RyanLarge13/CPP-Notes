c657da1ce469e5e3084a81a564294f4536afaec7
= <ryanhudsonlarge13@gmail.com>
2024-10-02 14:18:10 -0400

    Update Profile Confgurations

--- includes/fileManager.h
+++ includes/fileManager.h
@@ -17,203 +17,198 @@ using namespace filesystem;
 #define FILEMANAGER_H
 
 class FileManager {
- private:
- public:
- struct DirVectorData {
-  string path;
-  int nestedDirCt;
-  int nestedFileCt;
-  DirVectorData(const string &p, int nd, int nf): path(p),
-  nestedDirCt(nd),
-  nestedFileCt(nf) {}
- };
-
- #ifdef _WIN32
- const string HOME_DIR = getenv("USERPROFILE");
- #elif defined(__APPLE__)
- const string HOME_DIR = getenv("HOME");
- #elif defined(__linux__)
- const string HOME_DIR = getenv("HOME");
- #elif defined(__ANDROID__)
- const string HOME_DIR = "/data/data/com.termux/files/home";
- #else
- const string HOME_DIR = getenv("HOME");
- #endif
-
- string currentDir = HOME_DIR;
-
- bool isCorrectExt(const directory_entry &entry) {
-  string extension = entry.path().extension().string();
-  if (extension == ".wn") {
-   return true;
-  }
-  return false;
- }
-
- void closeFile(fstream *file) {
-  file->close();
- }
-
- bool checkDirExists(const string &dirPath) {
-  if (exists(dirPath)) {
-   return true;
-  }
-  return false;
- }
-
- pair < vector < DirVectorData >,
- vector < string>> grabDirsAndFiles() {
-  vector < DirVectorData > dirs;
-  vector < string > files;
-  path currentPath = current_path();
-  for (const auto &entry: directory_iterator(currentPath)) {
-   int nestedDirsCt = 0;
-   int nestedFilesCt = 0;
-   if (is_directory(entry.status())) {
-    for (const auto &item: directory_iterator(entry.path())) {
-     if (is_directory(item.status())) {
-      nestedDirsCt++;
-     }
-     if (is_regular_file(item.status()) && isCorrectExt(item)) {
-      nestedFilesCt++;
-     }
-    }
-    dirs.emplace_back(entry.path().filename().string(), nestedDirsCt,
-     nestedFilesCt);
-   }
-   if (is_regular_file(entry.status()) && isCorrectExt(entry)) {
-    files.push_back(entry.path().filename().string());
-   }
-  }
-  return {
-   dirs,
-   files
+private:
+public:
+  struct DirVectorData {
+    string path;
+    int nestedDirCt;
+    int nestedFileCt;
+    DirVectorData(const string &p, int nd, int nf)
+        : path(p), nestedDirCt(nd), nestedFileCt(nf) {}
   };
- }
 
- string getCurrentPath() {
-  path currentPath = current_path();
-  return currentPath;
- }
+#ifdef _WIN32
+  const string HOME_DIR = getenv("USERPROFILE");
+#elif defined(__APPLE__)
+  const string HOME_DIR = getenv("HOME");
+#elif defined(__linux__)
+  const string HOME_DIR = getenv("HOME");
+#elif defined(__ANDROID__)
+  const string HOME_DIR = "/data/data/com.termux/files/home";
+#else
+  const string HOME_DIR = getenv("HOME");
+#endif
 
- bool navDir(const string &dirPath) {
-  if (!checkDirExists(dirPath)) {
-   cout << "Does not exist returning from change dir" << endl;
-   return false;
-  }
-  try {
-   if (chdir(dirPath.c_str()) != 0) {
-    cout << "Error changing dir " << strerror(errno) << endl;
+  string currentDir = HOME_DIR;
+
+  bool isCorrectExt(const directory_entry &entry) {
+    string extension = entry.path().extension().string();
+    if (extension == ".wn") {
+      return true;
+    }
     return false;
-   }
-  } catch (filesystem_error &err) {
-   cout << "catch block err: " << err.what() << endl;
-   return false;
   }
-  currentDir = getCurrentPath();
-  return true;
- }
 
- string navBack() {
-  path current = getCurrentPath();
-  path parent = current.parent_path();
-  current_path(parent);
-  currentDir = getCurrentPath();
-  return current_path().filename().string();
- }
+  void closeFile(fstream *file) { file->close(); }
 
- bool navigateDir(const string &dirPath) {
-  string absolutePath = HOME_DIR + dirPath;
-  if (!checkDirExists(absolutePath)) {
-   cout << "Does not exist returning from change dir" << endl;
-   return false;
-  }
-  try {
-   if (chdir(absolutePath.c_str()) != 0) {
-    cout << "Error changing dir " << strerror(errno) << endl;
+  bool checkDirExists(const string &dirPath) {
+    if (exists(dirPath)) {
+      return true;
+    }
     return false;
-   }
-  } catch (filesystem_error &err) {
-   cout << "catch block err: " << err.what() << endl;
-   return false;
   }
-  currentDir = getCurrentPath();
-  return true;
- }
 
- ifstream *checkExistingFile(const string &fileName) {
-  ifstream *file = new ifstream(fileName);
-  if (!file->is_open()) {
-   delete file;
-   return nullptr;
+  pair<vector<DirVectorData>, vector<string>> grabDirsAndFiles() {
+    vector<DirVectorData> dirs;
+    vector<string> files;
+    path currentPath = current_path();
+    for (const auto &entry : directory_iterator(currentPath)) {
+      int nestedDirsCt = 0;
+      int nestedFilesCt = 0;
+      if (is_directory(entry.status())) {
+        for (const auto &item : directory_iterator(entry.path())) {
+          if (is_directory(item.status())) {
+            nestedDirsCt++;
+          }
+          if (is_regular_file(item.status()) && isCorrectExt(item)) {
+            nestedFilesCt++;
+          }
+        }
+        dirs.emplace_back(entry.path().filename().string(), nestedDirsCt,
+                          nestedFilesCt);
+      }
+      if (is_regular_file(entry.status()) && isCorrectExt(entry)) {
+        files.push_back(entry.path().filename().string());
+      }
+    }
+    return {dirs, files};
   }
-  return file;
- }
 
- fstream *openFileReadWrite(const string &fileName) {
-  try {
-   fstream *file = new fstream(fileName, ios::in | ios::out);
-   if (!file->is_open()) {
-    cout << "File not opened" << endl;
-    cout << strerror(errno) << endl;
-    delete file;
-    return nullptr;
-   }
-   return file;
-  } catch (filesystem_error &err) {
-   std::cout << "Error opening" << fileName << "\n" << "Error: " << err.what()
-   << "\n";
-   return nullptr;
+  string getCurrentPath() {
+    path currentPath = current_path();
+    return currentPath;
   }
- }
 
- ofstream *writeFile(const vector < string > &rows, ofstream *file) {
-  if (!file || !file->is_open()) {
-   delete file;
-   return nullptr;
-  }
-  for (const string &row: rows) {
-   *file << row << "\n";
-  }
-  return file;
- }
-
- ofstream *createNewFile(const string &fileName) {
-  ofstream *newFile = new ofstream(fileName);
-  if (!newFile->is_open()) {
-   delete newFile;
-   return nullptr;
-  }
-  return newFile;
- }
-
- bool createNewDir(const string &path) {
-  try {
-   if (create_directory(HOME_DIR + path)) {
+  bool navDir(const string &dirPath) {
+    if (!checkDirExists(dirPath)) {
+      cout << "Does not exist returning from change dir" << endl;
+      return false;
+    }
+    try {
+      if (chdir(dirPath.c_str()) != 0) {
+        cout << "Error changing dir " << strerror(errno) << endl;
+        return false;
+      }
+    } catch (filesystem_error &err) {
+      cout << "catch block err: " << err.what() << endl;
+      return false;
+    }
+    currentDir = getCurrentPath();
     return true;
-   }
-   return false;
-  } catch (filesystem_error &err) {
-   cout << endl << "sys err " << err.what() << endl;
-   return false;
   }
- }
 
- bool deleteFile(const string &fileName) {
-  if (fileName.empty()) {
-   return false;
-  } else {
-   if (remove(fileName.c_str()) != 0) {
-    return false;
-   } else {
+  string navBack() {
+    path current = getCurrentPath();
+    path parent = current.parent_path();
+    current_path(parent);
+    currentDir = getCurrentPath();
+    return current_path().filename().string();
+  }
+
+  bool navigateDir(const string &dirPath) {
+    string absolutePath = HOME_DIR + dirPath;
+    if (!checkDirExists(absolutePath)) {
+      cout << "Does not exist returning from change dir" << endl;
+      return false;
+    }
+    try {
+      if (chdir(absolutePath.c_str()) != 0) {
+        cout << "Error changing dir " << strerror(errno) << endl;
+        return false;
+      }
+    } catch (filesystem_error &err) {
+      cout << "catch block err: " << err.what() << endl;
+      return false;
+    }
+    currentDir = getCurrentPath();
     return true;
-   }
   }
- }
 
- void updateFileWeb() {}
- void deleteFileWeb() {}
- void createFileWeb() {}
+  ifstream *checkExistingFile(const string &fileName) {
+    ifstream *file = new ifstream(fileName);
+    if (!file->is_open()) {
+      delete file;
+      return nullptr;
+    }
+    return file;
+  }
+
+  fstream *openFileReadWrite(const string &fileName) {
+    try {
+      fstream *file = new fstream(fileName, ios::in | ios::out);
+      if (!file->is_open()) {
+        cout << "File not opened" << endl;
+        cout << strerror(errno) << endl;
+        delete file;
+        return nullptr;
+      }
+      return file;
+    } catch (filesystem_error &err) {
+      std::cout << "Error opening" << fileName << "\n"
+                << "Error: " << err.what() << "\n";
+      return nullptr;
+    }
+  }
+
+  ofstream *writeFile(const vector<string> &rows, ofstream *file) {
+    if (!file || !file->is_open()) {
+      delete file;
+      return nullptr;
+    }
+    for (const string &row : rows) {
+      *file << row << "\n";
+    }
+    return file;
+  }
+
+  ofstream *createNewFile(const string &fileName) {
+    ofstream *newFile = new ofstream(fileName);
+    if (!newFile->is_open()) {
+      delete newFile;
+      return nullptr;
+    }
+    return newFile;
+  }
+
+  bool createNewDir(const string &path) {
+    try {
+      if (create_directory(HOME_DIR + path)) {
+        return true;
+      }
+      return false;
+    } catch (filesystem_error &err) {
+      cout << endl << "sys err " << err.what() << endl;
+      return false;
+    }
+  }
+
+  bool renameDir(const string &dirname) { return false; }
+
+  bool deleteFile(const string &fileName) {
+    if (fileName.empty()) {
+      return false;
+    } else {
+      if (remove(fileName.c_str()) != 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  void updateFileWeb() {}
+  void deleteFileWeb() {}
+  void createFileWeb() {}
 };
 
 #endif
